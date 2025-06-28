9fa0b5ac84927dede58432528981da383c1b5ac1
Aniruddha Basak <codewithaniruddha@gmail.com>
2025-06-29 00:52:02 +0200

    Add stackit cluster fields

--- api/v1alpha1/stackitcluster_types.go
+++ api/v1alpha1/stackitcluster_types.go
@@ -17,25 +17,86 @@ limitations under the License.
 package v1alpha1
 
 import (
+	healthcheckconfigv1alpha1 "github.com/gardener/gardener/extensions/pkg/apis/config/v1alpha1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	componentbaseconfigv1alpha1 "k8s.io/component-base/config/v1alpha1"
 )
-
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
 // STACKITClusterSpec defines the desired state of STACKITCluster.
 type STACKITClusterSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// ClientConnection specifies the kubeconfig file and client connection
+	// settings for the proxy server to use when communicating with the apiserver.
+	// +optional
+	ClientConnection *componentbaseconfigv1alpha1.ClientConnectionConfiguration `json:"clientConnection,omitempty"`
+	// ETCD is the etcd configuration.
+	ETCD ETCD `json:"etcd"`
+	// HealthCheckConfig is the config for the health check controller
+	// +optional
+	HealthCheckConfig *healthcheckconfigv1alpha1.HealthCheckConfig `json:"healthCheckConfig,omitempty"`
+	// StackitRegion sets the STACKIT region.
+	StackitRegion string `json:"stackitRegion,omitempty"`
+	// StackitAPIEndpoints is the config for STACKIT API Endpoints
+	// +optional
+	StackitAPIEndpoints *StackitAPIEndpoints `json:"stackitAPIEndpoints,omitempty"`
 
-	// Foo is an example field of STACKITCluster. Edit stackitcluster_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// RegistryCaches optionally configures a container registry cache(s) that will be
+	// configured on every shoot machine at boot time (and reconciled while its running).
+	// +optional
+	RegistryCaches []RegistryCacheConfiguration `json:"registryCaches,omitempty"`
+}
+
+// ETCD is an etcd configuration.
+type ETCD struct {
+	// ETCDStorage is the etcd storage configuration.
+	Storage ETCDStorage `json:"storage"`
+	// ETCDBackup is the etcd backup configuration.
+	Backup ETCDBackup `json:"backup"`
+}
+
+// ETCDStorage is an etcd storage configuration.
+type ETCDStorage struct {
+	// ClassName is the name of the storage class used in etcd-main volume claims.
+	// +optional
+	ClassName *string `json:"className,omitempty"`
+	// Capacity is the storage capacity used in etcd-main volume claims.
+	// +optional
+	Capacity *resource.Quantity `json:"capacity,omitempty"`
+}
+
+// ETCDBackup is an etcd backup configuration.
+type ETCDBackup struct {
+	// Schedule is the etcd backup schedule.
+	// +optional
+	Schedule *string `json:"schedule,omitempty"`
+}
+
+// StackitAPIEndpoints contains all STACKIT API Endpoints.
+type StackitAPIEndpoints struct {
+	// LoadBalancer is the Endpoint of the LoadBalancer API.
+	// +optional
+	Loadbalancer *string `json:"loadbalancer,omitempty"`
+	// Token is the Token endpoint.
+	// +optional
+	Token *string `json:"token,omitempty"`
+}
+
+// RegistryCacheConfiguration configures a single registry cache.
+type RegistryCacheConfiguration struct {
+	// Server is the URL of the upstream registry.
+	Server string `json:"server"`
+	// Cache is the URL of the cache registry.
+	Cache string `json:"cache"`
+	// CABundle optionally specifies a CA Bundle to trust when connecting to the cache registry.
+	CABundle []byte `json:"caBundle,omitempty"`
+	// Capabilities optionally specifies what operations the cache registry is capable of.
+	Capabilities []string `json:"capabilities,omitempty"`
 }
 
 // STACKITClusterStatus defines the observed state of STACKITCluster.
 type STACKITClusterStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// Ready indicate STACKIT cluster is ready.
+	Ready bool `json:"ready,omitempty"`
 }
 
 // +kubebuilder:object:root=true
